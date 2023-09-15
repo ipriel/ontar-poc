1fcfcf6f9ed857ca76235f1857f8425d3060fa7c
nir-ontar <143128336+nir-ontar@users.noreply.github.com>
2023-09-15 11:55:39 +0300

    change broom color

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -248,7 +248,7 @@ const Chat = () => {
                                 >
                                 <BroomRegular
                                     className={styles.clearChatBroom}
-                                    style={{ background: isLoading || answers.length === 0 ? "#BDBDBD" : "radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)", 
+                                    style={{ background: isLoading || answers.length === 0 ? "#BDBDBD" : "radial-gradient(109.81% 107.82% at 100.1% 90.19%, #000000 0%, #888888 70.31%, #FFFFFF 100%)", 
                                             cursor: isLoading || answers.length === 0 ? "" : "pointer"}}
                                     aria-hidden="true"
                                 />
